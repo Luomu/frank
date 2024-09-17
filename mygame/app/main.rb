7b7d79d516b2dbfc91bde17c67de62e5e12a6787
Kimmo Kotajärvi <kimmo.kotajarvi@gmail.com>
2024-09-17 10:48:22 -0700

    Moving player, infinitely spawning enemies, game loop that can be lost but not won

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,85 +1,132 @@
+# Constants
+PLAYER_HEALTH     = 5
+PLAYER_MOVE_SPEED = 5
+ENEMY_MOVE_SPEED  = 3
+SCORE_PER_KILL    = 10
+
+def init args
+  args.state.player  = {x: 600, y: 320, w: 80, h: 80, path: 'sprites/circle/white.png', vx: 0, vy: 0, health: PLAYER_HEALTH, cooldown: 0, score: 0}
+  args.state.enemies = []
+  puts "Initialized"
+end
+
 def tick args
-  args.state.logo_rect ||= { x: 576,
-                             y: 200,
-                             w: 128,
-                             h: 101 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 600,
-                            text: 'Hello World!',
-                            size_px: 30,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 510,
-                            text: "Documentation is located under the ./docs/docs.txt directory. 150+ samples are located under the ./samples directory.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 480,
-                            text: "If you prefer formatted docs, you can access them locally at http://localhost:9001 or online at http://docs.dragonruby.org.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 400,
-                            text: "The code that powers what you're seeing right now is located at ./mygame/app/main.rb.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 380,
-                            text: "(you can change the code while the app is running and see the updates live)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.sprites << { x: args.state.logo_rect.x,
-                            y: args.state.logo_rect.y,
-                            w: args.state.logo_rect.w,
-                            h: args.state.logo_rect.h,
-                            path: 'dragonruby.png',
-                            angle: Kernel.tick_count }
-
-  args.outputs.labels  << { x: 640,
-                            y: 180,
-                            text: "(use arrow keys to move the logo around)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 80,
-                            text: 'Join the Discord Server! https://discord.dragonruby.org',
-                            size_px: 30,
-                            anchor_x: 0.5 }
-
-  if args.inputs.keyboard.left
-    args.state.logo_rect.x -= 10
-  elsif args.inputs.keyboard.right
-    args.state.logo_rect.x += 10
+  # Initialize/reinitialize
+  if !args.state.initialized
+    init args
+    args.state.initialized = true
   end
 
-  if args.inputs.keyboard.down
-    args.state.logo_rect.y -= 10
-  elsif args.inputs.keyboard.up
-    args.state.logo_rect.y += 10
+  # Execute current game state
+  if args.state.current_state
+    args.state.current_state.args = args
+    args.state.current_state.tick
+  else
+    # Not refactored into state yet
+    spawn_enemies args
+    collide_enemies args
+    move_enemies args
+    move_player args
+    # player attack
+
+    # Game over check
+    if args.state.player.health <= 0
+      args.state.current_state = State_Gameover.new args
+    end
+
+    # Render characters
+    args.outputs.sprites << [args.state.player, args.state.enemies]
+
+    render_hud args
   end
 
-  if args.state.logo_rect.x > 1280
-    args.state.logo_rect.x = 0
-  elsif args.state.logo_rect.x < 0
-    args.state.logo_rect.x = 1280
+  # Debug keys
+  # Reset
+  if args.inputs.keyboard.key_down.r
+    $gtk.reset_next_tick
   end
 
-  if args.state.logo_rect.y > 720
-    args.state.logo_rect.y = 0
-  elsif args.state.logo_rect.y < 0
-    args.state.logo_rect.y = 720
+  # Debug stats
+  args.outputs.labels << { x: 10, y: 70.from_top, r: 255, g: 255, b: 255, size_enum: -2, text: "FPS: #{args.gtk.current_framerate.to_sf}" }
+end
+
+def spawn_enemies args
+  # Spawn enemies more frequently as the player's score increases.
+  if rand < (100+args.state.player[:score])/(10000 + args.state.player[:score]) || Kernel.tick_count.zero?
+    theta = rand * Math::PI * 2
+    args.state.enemies << {
+        x: 600 + Math.cos(theta) * 800, y: 320 + Math.sin(theta) * 800, w: 80, h: 80,
+        path: 'sprites/circle/white.png',
+        r: (256 * rand).floor, g: (256 * rand).floor, b: (256 * rand).floor
+    }
+  end
+end
+
+# Collide enemies with player (they die in 1 hit and damage player)
+def collide_enemies args
+  args.state.enemies.reject! do |enemy|
+    # Check if enemy and player are within 80 pixels of each other (i.e. overlapping)
+    if 6400 > (enemy.x - args.state.player.x) ** 2 + (enemy.y - args.state.player.y) ** 2
+      # Enemy is touching player. Kill enemy, and reduce player HP by 1.
+      args.state.player[:health] -= 1
+      give_score SCORE_PER_KILL
+    else
+      # Player bullet/attack collisions
+    end
+  end
+end
+
+# Move enemies towards player. Use the current grid cell's vector
+# to move towards player's location, at close range use more precise
+# Direction towards player.
+def move_enemies args
+  args.state.enemies.each do |enemy|
+    # Get the angle from the enemy to the player
+    theta   = Math.atan2(enemy.y - args.state.player.y, enemy.x - args.state.player.x)
+    # Convert the angle to a vector pointing at the player
+    dx, dy  = theta.to_degrees.vector ENEMY_MOVE_SPEED
+    # Move the enemy towards thr player
+    enemy.x -= dx
+    enemy.y -= dy
+  end
+end
+
+def move_player args
+  if args.inputs.directional_angle
+    args.state.player.x += args.inputs.directional_angle.vector_x * PLAYER_MOVE_SPEED
+    args.state.player.y += args.inputs.directional_angle.vector_y * PLAYER_MOVE_SPEED
+    #args.state.player.x  = args.state.player.x.clamp(0, args.state.world.w - args.state.player.size)
+    #args.state.player.y  = args.state.player.y.clamp(0, args.state.world.h - args.state.player.size)
+  end
+end
+
+def render_hud args
+  args.outputs.labels << { x: 10, y: 90.from_top,
+    r: 255, g: 255, b: 255, size_enum: -2,
+    text: "Health: #{args.state.player.health}"
+  }
+
+  args.outputs.labels << { x: 100, y: 90.from_top,
+    r: 255, g: 255, b: 255, size_enum: -2,
+    text: "Score: #{args.state.player.score}"
+  }
+end
+
+def give_score amount
+  $args.state.player.score += amount
+end
+
+class State_Gameover
+  attr_gtk
+
+  def initialize args
+    self.args = args
+  end
+
+  def tick
+    outputs.labels << { x: 140, y: 130.from_top,
+      r: 255, g: 255, b: 49, size_enum: 2,
+      text: "GEIM OVER, HIT R TO RESTART"
+    }
   end
 end
